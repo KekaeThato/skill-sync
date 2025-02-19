fff8772547f152afa1f5ce8c2b3013c71a158d32
thkekaejhb024 <thkekaejhb024@student.wethinkcode.co.za>
2025-02-19 15:27:31 +0200

    still more work to do

--- code_clinic_auth.py
+++ code_clinic_auth.py
@@ -5,25 +5,31 @@ firebaseConfig = {"apiKey": "AIzaSyDFui7RW6Xy3yJSIt-dG1qhQPso2u17GVw",
                   "projectId": "code-clinic-a34a2",
                   "storageBucket": "code-clinic-a34a2.firebasestorage.app",
                   "messagingSenderId": "472731779092",
-                  "appId": "1:472731779092:web:28c13b339e6d8360a3e701",
-                  "measurementId": "G-LDJ6LNXP0K",
-                  "databaseURL": ""
-                  }
+                  "appId": "1:472731779092:web:1b0627769e14b831a3e701",
+                  "measurementId": "G-9P9WC36LQ7",
+                  "databaseURL": "https://code-clinic-a34a2-default-rtdb.firebaseio.com"
+                }
 
 #set up application i.e connect to our firebase project
 firebase = pyrebase.initialize_app(firebaseConfig)
 
 #to use the Authenticatio part of the project
 auth = firebase.auth()
+data_base = firebase.database()
 
 def sign_up()->None:
     """
     This function creates a new user
     """
-    email = input("Enter your email : ")
-    password = input("Enter your password : ")
     try:
+        name = input("Enter your name : ")
+        email = input("Enter your email : ")
+        password = input("Enter your password : ")
+        role = input("What's your role i.e Mentor/Peer : ")
+        expertise = input("what's your expertise : ")
         user = auth.create_user_with_email_and_password(email, password)
+        data = {"name": name, "email": email, "role": role, "expertise": expertise}
+        data_base.child("Users").push(data)
     except:
         print("Email already exist")
 
@@ -37,5 +43,15 @@ def log_in():
         user = auth.sign_in_with_email_and_password(email, password)
     except:
         print("Invalid password/Email")
+# def add_users()
+def main():
+    user_input = input("Do you have an account ? (y/n)").lower()
+    if user_input == "y":
+        log_in()
+    elif user_input == "n":
+        sign_up()
+    else:
+        return "wrong input try again!"
 
-sign_up()
+if __name__ == "__main__":
+    main()
