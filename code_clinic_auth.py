b5a10c677beb369623433719f49525095b96f3cd
thkekaejhb024 <thkekaejhb024@student.wethinkcode.co.za>
2025-02-24 08:55:18 +0200

    updated my code a little

--- code_clinic_auth.py
+++ code_clinic_auth.py
@@ -1,14 +1,7 @@
-import pyrebase
+import pyrebase, click, time
+from config import FIREBASECONFIG
 
-firebaseConfig = {"apiKey": "AIzaSyDFui7RW6Xy3yJSIt-dG1qhQPso2u17GVw",
-                  "authDomain": "code-clinic-a34a2.firebaseapp.com",
-                  "projectId": "code-clinic-a34a2",
-                  "storageBucket": "code-clinic-a34a2.firebasestorage.app",
-                  "messagingSenderId": "472731779092",
-                  "appId": "1:472731779092:web:1b0627769e14b831a3e701",
-                  "measurementId": "G-9P9WC36LQ7",
-                  "databaseURL": "https://code-clinic-a34a2-default-rtdb.firebaseio.com"
-                }
+firebaseConfig = FIREBASECONFIG
 
 #set up application i.e connect to our firebase project
 firebase = pyrebase.initialize_app(firebaseConfig)
@@ -22,36 +15,60 @@ def sign_up()->None:
     This function creates a new user
     """
     try:
-        name = input("Enter your name : ")
-        email = input("Enter your email : ")
-        password = input("Enter your password : ")
-        role = input("What's your role i.e Mentor/Peer : ")
-        expertise = input("what's your expertise : ")
+        name = click.prompt("Enter your name ")
+        email = click.prompt("Enter your email ")
+        password = click.prompt("Enter your password ")
+        role = click.prompt("What's your role i.e Mentor/Peer ")
+        expertise = click.prompt("what's your expertise ")
         user = auth.create_user_with_email_and_password(email, password)
         data = {"name": name, "email": email, "role": role, "expertise": expertise}
         data_base.child("Users").push(data)
+        click.echo("\nSuccessfully signed up,Now login")
+        log_in()
     except:
-        print("Email already exist")
+        click.echo("Email already exist")
 
-def log_in():
+def log_in()->None:
     """
     This function logs in if the user exists else throws an exceptions
     """
-    email = input("Enter your email : ")
-    password = input("Enter your password : ")
+    email = click.prompt("Enter your email ")
+    password = click.prompt("Enter your password ")
     try:
         user = auth.sign_in_with_email_and_password(email, password)
+        click.echo("\nSuccessfully logged in")
     except:
-        print("Invalid password/Email")
-# def add_users()
-def main():
-    user_input = input("Do you have an account ? (y/n)").lower()
+        click.echo("Invalid password/Email")
+def update_users(user:str)->None:
+    pass
+
+def view_workshop():
+    pass
+
+def request_meeting(requestor:str)->None:
+
+    topic = click.prompt("Enter the topic you want assistance in ")
+    date_requested = click.prompt("Enter the date you want your meet in this format(day/month/year)")
+    data ={"requestor_id":requestor, "topic":topic, "date_requested":date_requested}
+    data_base.child("Meetings").push(data)
+
+
+
+def view_bookings():
+    pass
+
+def cancel_booking():
+    pass
+
+
+def main()->None:
+    user_input = click.prompt("Do you have an account ? (y/n)")
     if user_input == "y":
         log_in()
     elif user_input == "n":
         sign_up()
     else:
-        return "wrong input try again!"
+        click.echo("wrong input try again!")
 
 if __name__ == "__main__":
     main()
---
+++ config.py
@@ -0,0 +1,9 @@
+FIREBASECONFIG = {"apiKey": "AIzaSyDFui7RW6Xy3yJSIt-dG1qhQPso2u17GVw",
+                  "authDomain": "code-clinic-a34a2.firebaseapp.com",
+                  "projectId": "code-clinic-a34a2",
+                  "storageBucket": "code-clinic-a34a2.firebasestorage.app",
+                  "messagingSenderId": "472731779092",
+                  "appId": "1:472731779092:web:1b0627769e14b831a3e701",
+                  "measurementId": "G-9P9WC36LQ7",
+                  "databaseURL": "https://code-clinic-a34a2-default-rtdb.firebaseio.com"
+                }
